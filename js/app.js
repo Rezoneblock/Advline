a556dea3ba5e6877acdab35b2fb95d6e6992c3ca
Timur <trew.tr2019@yandex.ru>
2021-03-15 19:48:50 +0300

    done slick-slider;

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-$(document).ready(function () {
-  $('.slider').slick();
-});
+// Get color from data-color.
+const setColor = (ele) => {
+  if (ele.data('color')) {
+    ele.closest('.main-slider').css('background-color', ele.data('color'));
+  }
+};
+
+// Slider.
+$(document)
+  .ready(function () {
+    $('.main-slider').slick({
+      fade: true,
+      cssEase: 'linear',
+      infinite: true,
+      dots: true,
+      arrows: false,
+      appendDots: $('.main__list__pagination'),
+    });
+  })
+  .on('init', (slick) => {
+    setColor($(slick.target).find('.slick-active'));
+  })
+  .on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+    setColor(slick.$slider.find(`[data-slick-index=${nextSlide}]`));
+  });
