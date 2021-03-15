01e7cce40eaf6ce2ac50f95636af9eddca9d1df0
Timur <trew.tr2019@yandex.ru>
2021-03-15 20:10:09 +0300

    final dist folder

---
+++ dist/js/app.js
@@ -0,0 +1,27 @@
+// Get color from data-color.
+const setColor = (ele) => {
+  if (ele.data('color')) {
+    ele.closest('.main-slider').css('background-color', ele.data('color'));
+  }
+};
+
+// Slider.
+$(document)
+  .ready(function () {
+    $('.main-slider').slick({
+      fade: true,
+      cssEase: 'linear',
+      infinite: true,
+      dots: true,
+      arrows: false,
+      appendDots: $('.main__list__pagination'),
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
+  })
+  .on('init', (slick) => {
+    setColor($(slick.target).find('.slick-active'));
+  })
+  .on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+    setColor(slick.$slider.find(`[data-slick-index=${nextSlide}]`));
+  });
